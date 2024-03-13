50fbf969b7d532071bc473bea2142c92e530af87
sevgeniy <ea9898@yandex.ru>
2024-03-13 22:19:04 +0300

    PP_2_1_3_SpringBeans решение

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,27 @@
 package app.config;
 
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
+    @Bean(name = "dog")
+    @Scope(scopeName = "prototype")
+    public Dog getCat() {
+        Dog dog = new Dog();
+        dog.setName("Nona");
+
+        return dog;
+    }
+
+    @Bean(name = "timer")
+    public Timer getTimer() {
+        return new Timer();
+    }
 }
---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,16 @@
+package app.model;
+
+public class Dog extends Animal {
+    private String name;
+
+    public Dog() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
